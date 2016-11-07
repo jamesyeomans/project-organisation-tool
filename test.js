973642d17e52cb8edbb031f0e70755f7ee82ad55
jamesyeomans <jamezyeomans@gmail.com>
2016-11-07 16:04:04 +0000

    add in test to fail

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-var value = 1;
-
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
